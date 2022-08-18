78da6e317aa35aab9bba8e2305ff5deeac7a0bc4
KanuBang <102508014+KanuBang@users.noreply.github.com>
2022-08-18 13:20:53 +0900

    2.3
    In the old version of JS, people use 'var' to use a variable. But the problem with 'var' is that programmers couldn't notice the updated point. So now we use 'const' or 'let' to use a variable. 'const' means the value should never be changed. As you can see, 'const' does not allow us to update. It allows us to only read. In contrast, 'let' means the value could be changed. Thanks to 'let', we can update the value freely. In conclusion, programmers can notice the purpose of each variable. So, you need to notice the purpose of a variable through 'let' and 'const' when you read code.

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const a  = 10;
 const b = 2;
-const name = "Messi";
-const clubName = " PSG1";
+let name = "Messi";
+let clubName = " PSG1";
 
 console.log(a+b);
 console.log(a*b);
 console.log(a/b);
 console.log(name + clubName);
+
+name = "Alaba";
+clubName = " AUS";
+console.log(name + clubName);
+
 
 
 
