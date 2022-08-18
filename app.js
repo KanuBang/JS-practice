6d2cefacfaadbcd7cd5e4bc6b3aa00c84e891ce9
KanuBang <102508014+KanuBang@users.noreply.github.com>
2022-08-18 12:45:33 +0900

    2.2
    Variables function saving and storing data. Const is similar to stay an original state. So if you make a variable in JS. Use const;
    And The way of making the name of a variable is camel case, snake case.
    myName is camel case and my_name is snake case. camel case is usually used in JS. and snake case is usually used in Python. And lastly, What does console.log( ); means?
    That means that print the data in '()' at the console.
    So i want you know that log means print.

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-alert("hi");
-alert("hello");
-alert("hello" + " Messi");
-alert(2+2);
+const a  = 10;
+const b = 2;
+const name = "Messi";
+const clubName = " PSG1";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log(name + clubName);
+
+
 
