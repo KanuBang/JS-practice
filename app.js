b28b8d0478d3e911bdc7b2c1a7123ff966177764
KanuBang <102508014+KanuBang@users.noreply.github.com>
2022-08-22 15:19:11 +0900

    3.6 CSS in Javascript
    The first point is the effect of variables.
    It is comfortable to understand intentions.
    
    The second point is to use conditionals. It is really easy to express various reactions.
    
    And the most important point we have to concentrate on is the process.
    
    We can bring the element that I want through querySeletor.
    And then we can realize the event related to the element through addEventListener or onEventName.
    
    And then we can order some reactions through functions when the event breaks out.
    
    In addition, you can use conditionals to represent various reactions.

--- app.js
+++ app.js
@@ -1,35 +1,17 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick ( ) {
-    h1.style.color ="blue";
-}
-function handleMouseEnter ( ) {
-    h1.innerText = "Mouse is here";
-}
-function handleMouseLeave( ) {
-    h1.innerText = "Mouse is gone";
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue") {
+        newColor = "tomato";
+    }
+
+    else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
-function handleWindowResize( ) {
-    document.body.style.backgroundColor = "tomato";
-}
-function handleWindowCopy( ) {
-    alert("Don't copy");
-}
-function handleWindowOffline ( ) {
-    alert("SOS no wifi");
-}
+h1.addEventListener("click", handleTitleClick);
 
-function handleWindowOnline ( ) {
-    alert ("connected");
-}
-
-h1.onclick = handleTitleClick;
-h1.onmouseenter = handleMouseEnter;
-h1.addEventListener("mouseleave", handleMouseLeave);
-h1.removeEventListener;
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
