e96f051a777ded44d634905a5339f03559d86e48
KanuBang <102508014+KanuBang@users.noreply.github.com>
2022-08-22 12:44:07 +0900

    3.1 Html in JavaScript
    The browser has the object that points to our website. This object is called a 'document'.
    And The Document method 'getElementById()' returns an Element object representing the element whose id property matches the specified string.
    In one word, Thanks to 'document.getElementById()", JavaScript can bring some informatinos from html.

--- app.js
+++ app.js
@@ -1,34 +1,7 @@
-const age = parseInt(prompt("Enter an age"));
+const title = document.getElementById("title");
+console.dir(title); /*객체의 데이터를 보고 싶을때 사용*/
 
-if(isNaN(age) || age <0) {
-    console.log("Please Enter a real postive number!");
-}
+title.innerText = "got you";
 
-else if (age < 18) {
-    console.log("You are too young!");
-}
-
-else if (age>=18 && age <50 ) {
-    console.log("Drink as much as you drink possible!");
-}
-
-else if (age>=50 && age <80) {
-    console.log("You can drink but drinking is not good for your age");
-}
-
-else if (age === 100) {
-    console.log("WOWWWWWW!");
-}
-
-else {
-    console.log("I know you quit drinking a long time ago");
-}
-
-/*
-javaScript can bring and chagne html.
-How?
-The browser gives you documets that have many objects.
-So The connection between javascript and html already exists through
-objects that browesr offered 
-
-*/
+console.log(title.id);
+console.log(title.className);
