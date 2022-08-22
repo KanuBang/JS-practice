6bcb373d4dc407bab490180cad5d9a67601eea05
KanuBang <102508014+KanuBang@users.noreply.github.com>
2022-08-22 13:56:53 +0900

    3.3 events
    function handleTitleClick ( ) {
        title.style.color ="blue";
    }
    title.addEventListener("click", handleTitleClick);
    
    This code shows well the effect of addEventListener.
    
    addEventListener is going to try to hear the events you added.
    In this case, the event you added is 'click'. If one user 'clicks', addEventListener will catch the event 'click'. And then addEventListern will execute the react you wrote.
    In this case, the react you wrote is the function 'handleTitleClick'.

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
-const hellos = document.getElementsByClassName("hello");
-console.log(hellos);
+const title = document.querySelector("div.hello:first-child h2");
 
-const title = document.getElementsByTagName("h1");
 console.log(title);
+console.dir(title);
 
-const title1 = document.querySelector(".hello h1");
-console.log(title1);
+function handleTitleClick ( ) {
+    title.style.color ="blue";
+}
 
-const title2 = document.querySelector(".hello h2");
-console.log(title2);
-
-const title3 = document.querySelectorAll(".hello h2");
-console.log(title3);
-
-const sameId1 = document.querySelector("#searchId");
-const sameId2 = document.getElementById("searchId");
-
-console.log(sameId1);
-console.log(sameId2);
-
-const change = document.querySelector(".hello h2");
-change.innerText = "change";
-console.log(change);
+title.addEventListener("click", handleTitleClick);
 
 
