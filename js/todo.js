0f1f1f55f693be87306a67ed393be14778a25827
KanuBang <102508014+KanuBang@users.noreply.github.com>
2022-08-26 15:27:34 +0900

    7.5 7.6 7.7 Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,8 +2,9 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 //const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
-const toDos = [];
+
 const TODOS_KEY = "todos";
+let toDos = [];
 
 function saveToDos( ) {
     localStorage.setItem(TODOS_KEY , JSON.stringify(toDos));
@@ -13,14 +14,16 @@ function saveToDos( ) {
 
 function deleteToDo(event) {
     const li = event.target.parentElement;
+    console.log(li.id);
     li.remove();
 }
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
     const button = document.createElement("button");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     button.innerText = "X";
     button.addEventListener("click", deleteToDo);
     li.appendChild(span);
@@ -32,8 +35,12 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+      };
+      toDos.push(newTodoObj);
+      paintToDo(newTodoObj);
     saveToDos(); 
 }
 
@@ -51,6 +58,47 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if(savedToDos !== null) {
     const parsedToDos = JSON.parse(savedToDos);
-    parsedToDos.forEach((item) => console.log("this is the turn of ", item ));
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
 }
 //this is arrow fucntion 
+
+/*
+function sexyFilter() {
+
+}
+
+[1,2,3,4].filter(sexyFilter);
+
+sexyFilter(1) = 1
+sexyFilter(2) = 2
+sexyFilter(3) x 3
+sexyFilter(4) = 4
+
+자바스크립트가 sexyFilter 1,2,3,4 부를 것
+*/
+
+/*
+filter practice
+const arr = ["pizza", "banana", "tomato"];
+function sexyFilter(food) {return food !== "banana"};
+arr.filter(sexyFilter);
+
+자바스크립트가 sexyFilte ["pizza", "banana", "tomato"]를 부르고
+food가 차례대로 "pizza", "banana", "tomato" 값을 가질 것
+만약 food !== "banana"나가 false를 리턴하면 그 foods는 새 array이에 포험되지 않을 것.
+*/
+
+/*
+filter practice
+const arr = [1234,5454,223,122,45,6775,334];
+function sexyFunction(potato) {return potato <= 1000};
+arr.filter(sexyFunction);
+*/
+
+/*
+filter practice
+const todos = [{text:"lalala"}, {text:"lololo"}];
+function sexyFilter(todo) {return todo.text !== "lololo"};
+todos.filter(sexyFilter);
+*/
